8b0fa62d98480b7aaf9186fc783276a9d0f91178
0kaydex <0kaydex@gmail.com>
2026-08-24 02:16:04 +0100

    modified code

--- losub-backendF/losub-backend/server.js
+++ losub-backendF/losub-backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
     "http://127.0.0.1:5500",
     "http://localhost:5500"
   ],
-  methods: ["GET", "POST", "PUT"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
