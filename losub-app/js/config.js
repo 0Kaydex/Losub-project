5fb948d83c8f74b1b8489a059fb91d403f044ea6
0kaydex <0kaydex0@gmail.com>
2026-09-03 13:49:19 +0100

    another commit

--- losub-app/js/config.js
+++ losub-app/js/config.js
@@ -3,4 +3,4 @@ const API_BASE_URL = "https://api.losubapp.com/api";
 const GOOGLE_CLIENT_ID = "342069621439-d4lt30bg3vffskon1gmf9p4v0ftgros3.apps.googleusercontent.com";
 
 // const PAYSTACK_PUBLIC_KEY = "pk_live_54aa197d81011d9e56ba5da9f0a3fbe6fa56f283"; // Paystack public key
-const PAYSTACK_PUBLIC_KEY = "pk_test_54aa197d81011d9e56ba5da9f0a3fbe6fa56f283"; // Paystack test public key
+const PAYSTACK_PUBLIC_KEY = "pk_test_3525cd3e6d74acf401311b8602ea6990bfb90683"; // Paystack test public key
